48526aa2f171ce1420c6ca4dbe2927dc171cc617
Milton Mamani <miltin.mamani010@gmail.com>
2026-03-31 21:14:14 -0300

    agregamos opciones de como agregar mensajes por pantallas

---
+++ src/01-variables.js
@@ -0,0 +1,17 @@
+// este es un comentario de una linea
+
+/* este es 
+un comentario 
+de varias lineas */
+// -------------- mensajes en consola y en la pagina web----------------
+console.log("Hola mundo 🌍😊"); //mensaje a la consola
+
+document.writeln("<p class='parrafoResaltado'>Hola Mi mundo 🌍😊</p>"); //mensaje a la pagina web
+
+document.writeln(
+  '<a href="http://127.0.0.1:5500/index.html">Mi página web</a>',
+); // se pueden agregar etiquetas html a la pagina web desde js y tambien ppueden trabajar con stylos css, como se muestra en el ejemplo de arriba, se le asigna una clase a la etiqueta p y luego se le da estilo a esa clase desde el archivo css.
+
+//----------- ventana emergente-----------
+
+alert("bienvenido a mi página web 💻😊"); //mensaje emergente en la pagina web
